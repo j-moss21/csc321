31b768dd674e1be8551e094d7f6ef5ac60322fb9
j-moss21 <mossjulen@gmail.com>
2025-02-12 05:28:06 +0000

    lab 4

---
+++ jmoss4.java
@@ -0,0 +1,26 @@
+//Julen Moss
+//
+//
+import java.util.Scanner;
+
+public class jmoss4 {
+
+	public static void main(String[] args) {
+		
+int x=0;
+
+System.out.println("Choose an option for best Disney sequel");
+System.out.println("Mulan II");
+System.out.println("Frozen II");
+System.out.println("Cinderella 3: A Twist in Time");
+System.out.println("The Little Mermaid II: Return to the Sea");
+
+Scanner userinput = new Scanner(System.in);
+x = userinput.nextInt();
+
+System.out.println("You choose option " +x);
+	}
+
+
+}
+
