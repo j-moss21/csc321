e7393aec013d30b545c5f8544dd4b9cdf1f5415b
j-moss21 <mossjulen@gmail.com>
2025-02-26 03:08:37 +0000

    Lab 7

---
+++ jmoss7.py
@@ -0,0 +1,29 @@
+#Julen Moss
+x = 10
+
+y = 7.6
+
+z = " Hello"
+
+if x > 9:
+    print(" x is greater than 10")
+elif x < 9:
+    print(" x is less than 9")
+else:
+    print(" x is 10")
+
+if y < 7:
+    print(" y is less than 7")
+elif y == 7.6:
+    print(" y is 7.6")
+elif y > 8:
+    print ("y is greater tnan 8")
+else:
+    print("y is between 6 and 8  but not 7")
+
+if z == " Hello ":
+    print(" Variable z is hello")
+else:
+    print("Variable z is not hello")
+
+
