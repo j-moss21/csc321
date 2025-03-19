d2a950a91963ebfb46eada0559c6c925a2908afd
j-moss21 <mossjulen@gmail.com>
2025-03-18 17:58:20 -0700

    Update jmoss10.java

--- jmoss10.java
+++ jmoss10.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class jmoss10 {
 
-    public static double calculateArea(double radius, double height) {
+    public static double calculateVolume(double radius, double height) {
 
-        return 2 * Math.PI * radius * (radius + height);
+        return Math.PI * Math.pow(radius, 2) * height;
     }
 
     public static void main(String[] args) {
@@ -16,13 +16,14 @@ public class jmoss10 {
         System.out.print("Enter the radius of the cylinder: ");
         double radius = scanner.nextDouble();
 
-       System.out.print("Enter the height of the cylinder: ");
+        System.out.print("Enter the height of the cylinder: ");
         double height = scanner.nextDouble();
+        
+        double volume = calculateVolume(radius, height);
 
-        double area = calculateArea(radius, height);
-
-        System.out.printf("The area of the cylinder is: %.2f%n", area);
+        System.out.printf("The volume of the cylinder is: %.2f cubic units%n", volume);
 
         scanner.close();
     }
 }
+
